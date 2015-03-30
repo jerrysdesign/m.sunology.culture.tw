0b612afa231597e1fec4aa4c30411d8952921bd6
jerrysdesign <jerry7722@hotmail.com>
2015-03-30 23:07:40 +0800

    base setting

--- project/source/config.rb
+++ project/source/config.rb
@@ -1,3 +1,7 @@
+# Require any additional compass plugins here.
+require 'slim'
+Slim::Engine.set_default_options :pretty => true, :disable_escape => true, :format => :html5
+
 # Set this to the root of your project when deployed:
 http_path = "/"
 css_dir = "stylesheets"
